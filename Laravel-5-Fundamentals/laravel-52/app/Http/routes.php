7ebfa9308b40c9ec845816ca46d8accfa708196b
crwh05 <Colin@Colins-MacBook-Pro.local>
2016-01-21 21:41:51 +0000

    Lesson 9
    $ php artisan make:controller ArticlesController

--- Laravel-5-Fundamentals/laravel-52/app/Http/routes.php
+++ Laravel-5-Fundamentals/laravel-52/app/Http/routes.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
 
 Route::get('about', 'PagesController@about');
 
+Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show');
 /*
 |--------------------------------------------------------------------------
 | Application Routes
