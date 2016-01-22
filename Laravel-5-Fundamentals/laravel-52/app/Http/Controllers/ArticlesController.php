18c656e62823b7842c065314275e023453534dc5
crwh05 <Colin@Colins-MacBook-Pro.local>
2016-01-22 12:02:11 +0000

    Lesson 10 - Forms
    10 - Forms
    
    $ composer require illuminate/html
    
    add providers and aliases to the config/app.php file
    
    		Illuminate\Html\HtmlServiceProvider::class,
    
            'Form'      => Illuminate\Html\FormFacade::class,
            'Html'      => Illuminate\Html\HtmlFacade::class,
    
    In Browser:
    FatalErrorException in HtmlServiceProvider.php line 36:
    Call to undefined method Illuminate\Foundation\Application::bindShared()
    
    https://laracasts.com/discuss/channels/laravel/call-to-undefined-method-
    illuminatefoundationapplicationbindshared
    
    /path-to-your-project/vendor/illuminate/html/HtmlServiceProvider.php
    
    https://atom.io/docs/latest/using-atom-snippets
    
    $ cd ~/.atom
    $ vi snippets.cson
    
    # blade
    '.text.html.php.blade':
      'textfield':
        'prefix': 'textfield'
        'body': """
          <!-- ${2:Name} Form Input -->
          <div class="form-group">
            {!! Form::label('${1:name}', '${2:Name}:') !!}
            {!! Form::text$4('${1:name}', ${3:null}, ['class' =>
    'form-control']) !!}
          </div>$5
        """

--- Laravel-5-Fundamentals/laravel-52/app/Http/Controllers/ArticlesController.php
+++ Laravel-5-Fundamentals/laravel-52/app/Http/Controllers/ArticlesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
+use Request;
+use Carbon\Carbon;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -12,7 +14,7 @@ class ArticlesController extends Controller
 {
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::latest()->get();
 
         return view('articles.index', compact('articles'));
     }
@@ -22,6 +24,21 @@ class ArticlesController extends Controller
         $article = Article::findOrFail($id);
 
         return view('articles.show', compact('article'));
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store()
+    {
+        $input = Request::all();
         
+        $input['published_at'] = Carbon::now();
+
+        Article::create($input);
+
+        return redirect('articles');
     }
 }
